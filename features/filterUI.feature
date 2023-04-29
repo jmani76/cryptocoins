f702d3b44c05101f6f24d84aeb7a126387907fad
Jaya Mani <jayalakshmi.mani@gmail.com>
2023-04-29 23:29:54 +0100

    part-1 of the test complete.

--- features/filterUI.feature
+++ features/filterUI.feature
@@ -1,13 +1,19 @@
 Feature: Filtering UI
+
   Description: This feature file mainly covers a user visiting the coin market cap website
   and apply relevant filters to narrow down the results and undertake comparison
   based on the page content.
 
-  Background:
-    Given I visit "https://coinmarketcap.com/" website
 
-
-  Scenario:
-    Given I'm on coin market cap website homepage
+  Scenario: Filtering UI and extracting data for comparison
+    Given I am on "https://coinmarketcap.com/" website
     When I limit the max rows to show at 20
     Then Only 20 rows are loaded
+    And I can capture the market cap performance parameters for the top 5 ranking cryptos
+    When I select algorithm as "PoW"
+    And Apply more filters as given below
+    |cryptoType|toggleMineableState|minPrice|maxPrice|
+    |coins     |On                 |100     |10000   |
+    Then I can capture the top 5 ranking cryptos from the filtered results
+    And Compare the same with unfiltered results captured earlier
+
--- features/steps/filterUI.steps.js
+++ features/steps/filterUI.steps.js
@@ -3,17 +3,16 @@ const { expect } = require("@playwright/test")
 
 const homepage = require("../../pages/homepage");
 
-// Background step implementation
-Given('I visit {string} website', async (url) => {
+let unfilteredResults;
+let filteredResults;
+
+// Scenario steps
+Given('I am on {string} website', async (url) => {
     await page.goto(url);
 
     if (process.env.COINMARKETCAP_CLOSE_HOMEPAGE_MODAL === 'true') {
         await page.click(homepage.locators.Modal_Dialog);
     }
-});
-
-// Scenario steps
-Given('I\'m on coin market cap website homepage', async () => {
     await expect(page).toHaveTitle(homepage.locators.Page_Title);
 });
 
@@ -25,4 +24,41 @@ When('I limit the max rows to show at {int}', async (number_of_rows) => {
 Then('Only {int} rows are loaded', async (number_of_rows) => {
     let status = await homepage.VerifyNumberOfRowsOnDisplay(number_of_rows);
     await expect(status).toBeTruthy();
+});
+
+Then('I can capture the market cap performance parameters for the top {int} ranking cryptos',
+    async(number_of_top_performers) => {
+    unfilteredResults = await homepage.CaptureMarketCapParameters(number_of_top_performers);
+    await expect(unfilteredResults.length).toEqual(number_of_top_performers);
+});
+
+When('I select algorithm as {string}', async (algorithm_type) => {
+    let status = await homepage.ChooseAnAlgorithm(algorithm_type);
+    await expect(status).toBeTruthy();
+});
+
+When('Apply more filters as given below', async (dataTable) => {
+    const filterData = dataTable.hashes();
+    let status = await homepage.SetMoreFilters(filterData[0].cryptoType, filterData[0].toggleMineableState,
+        filterData[0].minPrice, filterData[0].maxPrice);
+    await expect(status).toBeTruthy();
+});
+
+Then('I can capture the top {int} ranking cryptos from the filtered results',
+    async(number_of_top_performers) => {
+    filteredResults = await homepage.CaptureMarketCapParameters(number_of_top_performers);
+    await expect(filteredResults.length).toEqual(number_of_top_performers);
+});
+
+Then('Compare the same with unfiltered results captured earlier', async() => {
+    let data = await homepage.CompareCapturedPageContents(unfilteredResults, filteredResults);
+    if (data.length > 0) {
+        console.log("Filtered row of results that are different to Unfiltered results");
+        console.log(data);
+        await expect(data.length).toBeGreaterThan(0);
+    }
+    else {
+        console.log("Unfiltered vs Filtered are same!");
+        await expect(data.length).toEqual(0);
+    }
 });
